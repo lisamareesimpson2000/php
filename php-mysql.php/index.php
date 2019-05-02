5c7a70af655ebd328f8ec98b065752b769447ab3
Lisa Simpson <lisamaree02@Lisas-MacBook-Pro.local>
2019-05-02 12:12:24 +1200

    added to

---
+++ php-mysql.php/index.php
@@ -0,0 +1,25 @@
+<?php
+$title ="Home";
+$content = "This is the description of my Home page";
+	require("templates/header.php");
+?>
+
+	<main>
+		<h1>Homepage</h1>
+			<p>About the website.</p>
+
+	</main>
+
+<?php
+	CREATE TABLE furniture (
+		column1 ID,
+		column2 Type,
+		column3 Price,
+		column4 colour,
+	   ....
+	);
+?>
+
+<?php
+	require("templates/footer.php");
+?>	
